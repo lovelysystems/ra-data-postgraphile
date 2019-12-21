8becd29be428ef4c80fc417f98d66b1447d1f1dd
abumalick <abdullah.hilson@gmail.com>
2019-12-21 22:58:12 +0000

    feat: allow to pass a client in pgDataProvider factory

--- src/factory.ts
+++ src/factory.ts
@@ -6,23 +6,24 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import resource from './resource'
 
 export type FactorySettings = {
+  client: ApolloClient
   uri: string
   cache?: any
   options: any
 }
 
-export const factory = ({ uri, cache, options }: FactorySettings) => {
+export const factory = ({ client, uri, cache, options }: FactorySettings) => {
   const defaultCache = () => {
     return new InMemoryCache({
       dataIdFromObject: (object: any) => object.nodeId || null,
     })
   }
-  const client = new ApolloClient({
+  const finalClient = client || new ApolloClient({
     uri,
     cache: cache || defaultCache(),
   })
   return buildGraphQLProvider({
-    client,
+    client: finalClient,
     buildQuery: resource,
     options,
   })
