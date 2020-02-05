fe97cada6b13e8435f0e6762f6cf4e75615736d2
Harald Friessnegger <friessnegger@lovelysystems.com>
2020-02-05 10:53:12 +0100

    fix test syntax and run prettier
    add prettier config that matches current styles

--- src/__tests__/field.test.ts
+++ src/__tests__/field.test.ts
@@ -1,102 +1,83 @@
 import merge from 'lodash/merge'
-import {
-  createQueryFromType,
-  SimpleFieldHandlers,
-} from '../field'
+import { createQueryFromType, SimpleFieldHandlers } from '../field'
 import { ContentType, ContentBlockType } from './helpers'
 
 describe('createQueryFromType', () => {
-
   it('provides simple field types', () => {
     expect(
-      createQueryFromType(
-        {
-          typeName: 'Content',
-          typeMap: {
-            Content: ContentType,
-            ContentBlock: ContentBlockType,
+      createQueryFromType({
+        typeName: 'Content',
+        typeMap: {
+          Content: ContentType,
+          ContentBlock: ContentBlockType
+        },
+        handlers: SimpleFieldHandlers,
+        settings: {
+          nodeId: true,
+          name: true,
+          blocks: true,
+          block: {
+            type: true
           },
-          handlers: SimpleFieldHandlers,
-          settings: {
-            nodeId: true,
-            name: true,
-            blocks: true,
-            block: {
-              type: true
-            },
-            '=de': {
-              arguments: 'filter: {lang: {equalTo: DE}}',
-              query: {
-                blocks: {
-                  type: true,
-                }
+          '=de': {
+            arguments: 'filter: {lang: {equalTo: DE}}',
+            query: {
+              blocks: {
+                type: true
               }
-            },
-            pubTs: true,
-            deleted: true,
-            id: true,
-            ts: true,
-            author: true,
-          }
+            }
+          },
+          pubTs: true,
+          deleted: true,
+          id: true,
+          ts: true,
+          author: true
         }
-      ),
+      })
     ).toStrictEqual(
-      ' de: blocks(filter: {lang: {equalTo: DE}}) {  type } author block {  type } blocks deleted id name nodeId pubTs ts',
+      ' de: blocks(filter: {lang: {equalTo: DE}}) {  type } author block {  type } blocks deleted id name nodeId pubTs ts'
     )
   })
 
   it('allows to define alias w/o arguments', () => {
     expect(
-      createQueryFromType(
-        {
-          typeName: 'Content',
-          typeMap: {
-            Content: ContentType,
-            ContentBlock: ContentBlockType,
-          },
-          handlers: SimpleFieldHandlers,
-          settings: {
-            '=effective': {
-              query: {
-                pubTs: true,
+      createQueryFromType({
+        typeName: 'Content',
+        typeMap: {
+          Content: ContentType,
+          ContentBlock: ContentBlockType
+        },
+        handlers: SimpleFieldHandlers,
+        settings: {
+          '=effective': {
+            query: {
+              pubTs: true
             }
           }
         }
-      ),
-    ).toStrictEqual(
-      ' effective: pubTs',
-    )
+      })
+    ).toStrictEqual(' effective: pubTs')
   })
 
   it('can be configured for object fields', () => {
-    const myFieldHandlers = merge(
-      {},
-      SimpleFieldHandlers,
-      {
-        nodeId: () => {
-          return 'changedNameForNodeId'
-        }
+    const myFieldHandlers = merge({}, SimpleFieldHandlers, {
+      nodeId: () => {
+        return 'changedNameForNodeId'
       }
-    )
+    })
     expect(
-      createQueryFromType(
-        {
-          typeName: 'Content',
-          typeMap: {
-            Content: ContentType,
-          },
-          handlers: myFieldHandlers,
-          settings: {
-            nodeId: true,
-            name: true,
-            id: true,
-          },
+      createQueryFromType({
+        typeName: 'Content',
+        typeMap: {
+          Content: ContentType
+        },
+        handlers: myFieldHandlers,
+        settings: {
+          nodeId: true,
+          name: true,
+          id: true
         }
-      ),
-    ).toStrictEqual(
-      ' id name changedNameForNodeId',
-    )
+      })
+    ).toStrictEqual(' id name changedNameForNodeId')
   })
-  })
-
 })
