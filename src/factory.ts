78f0e9e31bc0deed20cd1806206258efb9a7666e
dobe <bernddorn@gmail.com>
2020-03-03 08:18:12 +0100

    wip: catch network errors and ingore json parser failures in gql client
    also just take the incomming apollolink as terminating and keep the error link

--- src/factory.ts
+++ src/factory.ts
@@ -1,43 +1,51 @@
 import buildGraphQLProvider from 'ra-data-graphql'
 
 import ApolloClient from 'apollo-client'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { HttpLink } from 'apollo-link-http'
-import { onError } from 'apollo-link-error'
-import { ApolloLink } from 'apollo-link'
+import {InMemoryCache} from 'apollo-cache-inmemory'
+import {HttpLink} from 'apollo-link-http'
+import {onError} from 'apollo-link-error'
+import {ApolloLink, Observable} from 'apollo-link'
 
 import resource from './resource'
+import {throwServerError} from 'apollo-link-http-common'
 
 export type FactorySettings = {
   uri?: string
-  link?: any
+  link?: ApolloLink
   cache?: any
   options: any
 }
 
-export const factory = ({ uri, link, cache, options, ...rest }: FactorySettings) => {
-  const defaultCache = () => {
-    return new InMemoryCache({
-      dataIdFromObject: (object: any) => object.nodeId || null,
-    })
+const errorLink = onError(({graphQLErrors, networkError}) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({message, locations, path}) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
+    )
+  if (networkError) {
+    console.log(`[Network error]: ${networkError}`)
+    if ('statusCode' in networkError) {
+      if ('bodyText' in networkError) {
+        return new Observable(obs => {
+          const response = {status: networkError.statusCode, ok: false}
+          throwServerError(response, 'ServerError', networkError.bodyText)
+        })
+      }
+    }
   }
-  const defaultLink = () => {
-    return ApolloLink.from([
-      onError(({ graphQLErrors, networkError }) => {
-        if (graphQLErrors)
-          graphQLErrors.forEach(({ message, locations, path }) =>
-            console.log(
-              `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-            ),
-          );
-        if (networkError) console.log(`[Network error]: ${networkError}`);
-      }),
-      new HttpLink({ uri: uri })
-    ])
-  }
+})
+
+
+export const factory = ({uri, link, cache, options, ...rest}: FactorySettings) => {
+  cache = cache || new InMemoryCache({
+    dataIdFromObject: (object: any) => object.nodeId || null,
+  })
+  link = ApolloLink.from([errorLink, link || new HttpLink({uri: uri})])
+
   const client = new ApolloClient({
-    cache: cache || defaultCache(),
-    link: link || defaultLink(),
+    cache: cache,
+    link: link,
     ...rest,
   })
   return buildGraphQLProvider({
